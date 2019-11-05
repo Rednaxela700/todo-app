6084736ecdebb6f9a37fe3a2fccf6fdb646cb7b4
rednaxela700 <ohleg@windowslive.com>
2019-11-05 14:04:34 +0100

    added dom elements to js variables

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const clear__icon = document.querySelector('.icon__container--clear'),
+    dateEl = document.getElementById('date'),
+    listEl = document.getElementById('list'),
+    inputEl = document.getElementById('note__input');
