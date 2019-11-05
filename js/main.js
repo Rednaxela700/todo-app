c12944754cd75e741952ac960d6cf7f9cfc5a3ea
rednaxela700 <ohleg@windowslive.com>
2019-11-05 14:14:01 +0100

    added addToDo function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ const clear__icon = document.querySelector('.icon__container--clear'),
     dateEl = document.getElementById('date'),
     listEl = document.getElementById('list'),
     inputEl = document.getElementById('note__input');
+
+function addToDo(toDo) {
+    const list__html = `<li class="list__item">
+                            <i class="fa fa-circle icon__item--complete" job="done"></i>
+                            <p class="list__paragraph">${toDo}</p>
+                            <i class="fa fa-trash-o icon__item--delete" job="delete"></i>
+                        </li>`;
+    const position = 'beforeend';
+    listEl.insertAdjacentHTML(position, list__html)
+}
