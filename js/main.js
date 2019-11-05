e58331ebb5ff0eb4b5b589503ab8af277a3e20e6
rednaxela700 <ohleg@windowslive.com>
2019-11-05 14:42:17 +0100

    added parameters to addToDo()

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ const clear__icon = document.querySelector('.icon__container--clear'),
     listEl = document.getElementById('list'),
     inputEl = document.getElementById('note__input');
 
+const el_style_check = 'fa-check-circle',
+    el_style_uncheck = 'fa-circle-thin',
+    el_style_lineThrough = 'lineThrough';
+
 let toDoList__arr = [];
+
 let id = 0;
 
 toDoList__arr[0] =
@@ -21,11 +26,15 @@ toDoList__arr[1] = {
     trash: false
 };
 
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+    if (trash){return} //if item is in the trash further code in this func won't work
+
+    const item__done = done ? el_style_check : el_style_uncheck;
+    const item__line = done ? el_style_lineThrough : '';
     const list__html = `<li class="list__item">
-                            <i class="fa fa-circle icon__item--complete" job="done"></i>
+                            <i class="fa fa-circle icon__item--complete" job="done" id="${id}"></i>
                             <p class="list__paragraph">${toDo}</p>
-                            <i class="fa fa-trash-o icon__item--delete" job="delete"></i>
+                            <i class="fa fa-trash-o icon__item--delete" job="delete" id="${id}"></i>
                         </li>`;
     const position = 'beforeend';
     listEl.insertAdjacentHTML(position, list__html)
